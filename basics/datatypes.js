b088863b31d651f34e47171f6a1393698a70aac2
Shubhamtelmasare <138513458+Shubhamtelmasare@users.noreply.github.com>
2024-12-24 16:18:28 +0000

    const var let and datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"//It will treat all JS code as newer version
+
+// alert(3+3) it works only in browser
+
+let name = "Shubham" // String
+let age = 21 // Integer/number
+let Value = true // Boolean
+let  val= 5.4 // float
+let name1 //undefined
+// null it is empty
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof Value)
+console.log(typeof val)
+console.log(typeof name1)
+console.log(typeof null)
---
+++ basics/variable_and_constant.js
@@ -0,0 +1,15 @@
+const accountId = 123
+let accountEmail = "Shubh@gmail.com"
+var accountPass = "Shubh"
+accountCity = "Nagpur"
+//accountId=121
+console.log(accountId)
+console.table([accountId, accountEmail, accountPass, accountCity])
+
+console.log("After Chaning")
+accountEmail = "Shubham@gmail.com"
+var accountPass = "Shubham"
+accountCity = "PardiNagpur"
+let accocuntPin
+//let accocuntPin; This is also valid
+console.table([accountId, accountEmail, accountPass, accountCity, accocuntPin])
