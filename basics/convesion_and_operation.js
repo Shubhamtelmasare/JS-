3c37ae9dbed48e21918c3268a69ac34021c9cdb2
Shubhamtelmasare <138513458+Shubhamtelmasare@users.noreply.github.com>
2024-12-24 16:44:39 +0000

    converion

---
+++ basics/convesion_and_operation.js
@@ -0,0 +1,22 @@
+let a = "11";
+console.log(typeof a);
+console.log(a);
+let valueInNumber = Number(a);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let c = true;
+let d = null;
+let e = undefined;
+let b = "11ab";
+console.log(typeof b);
+console.log(a);
+let valueInNumber1 = Number(b);
+let valueInNumber2 = Number(c);
+let valueInNumber3 = Number(d);
+let valueInNumber4 = Number(e);
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1); //it will print NaN means Not a Number
+console.log(valueInNumber2);
+console.log(valueInNumber3);
+console.log(valueInNumber4);
