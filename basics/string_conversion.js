cf0662df47c8947f9c3cbde61cd54d3129e16ce2
Shubhamtelmasare <138513458+Shubhamtelmasare@users.noreply.github.com>
2024-12-24 17:15:34 +0000

    string conversion

---
+++ basics/string_conversion.js
@@ -0,0 +1,16 @@
+let a="string1"
+let b=" string2"
+console.log(a+b)
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+//console.log("1"+2);
+console.log(true);
+console.log(+true);
+console.log(+"")
+
+let c=1;
+console.log(++c);
+console.log(c++);
