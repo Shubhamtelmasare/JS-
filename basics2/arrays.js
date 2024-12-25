6d661fa881415587c072b47006c7688dd98acb9c
Shubhamtelmasare <138513458+Shubhamtelmasare@users.noreply.github.com>
2024-12-25 16:18:20 +0000

    arrays

---
+++ basics2/arrays.js
@@ -0,0 +1,23 @@
+// const arr=new Array(1,2,3,4,5,5)
+// const myarr = [1,2,3,4,5,6,7,'s',"Shubham"]
+// console.log(myarr)
+// myarr.push(8)
+// console.log(myarr)
+// myarr.pop()
+// console.log(myarr)
+// console.log(myarr.includes(4))
+// console.log(myarr.indexOf(4))
+
+// const newarr=myarr.join()
+// console.log(newarr)
+// console.log(typeof(newarr))
+
+// const orgarr = [1,3,4,5,657,4,32,4,35]
+// console.log('A',orgarr)
+// const orgarr1 =orgarr.slice(1,3)
+// console.log('B',orgarr)
+// console.log('B',orgarr1)
+// const orgarr2 =orgarr.splice(1,3)
+// console.log('C',orgarr)
+// console.log('C',orgarr2)
+
