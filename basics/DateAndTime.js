b29e3edab3ff383dc24fe794580caf8b44aed698
Shubhamtelmasare <138513458+Shubhamtelmasare@users.noreply.github.com>
2024-12-25 15:09:32 +0000

    Date and Time

---
+++ basics/DateAndTime.js
@@ -0,0 +1,17 @@
+// let myDate = new Date();
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof Date)
+// console.log(typeof myDate)
+
+let createDate = new Date(2025, 0, 1)
+let anotherDate = new Date(2025, 0, 2, 5, 3)
+let extraeDate = new Date(2025, 0, 3, 14,50)
+console.log(createDate.toLocaleString())
+console.log(anotherDate.toString())
+console.log(extraeDate.toString())
+let jan14=new Date("01-14-2025")
+let anotherjan14=new Date("2025-01-14")
+console.log(jan14.toString())
